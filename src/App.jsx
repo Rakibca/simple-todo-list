22bd0db690369eda0eb87b07c5d2b8d8a8730ee6
Rakibul Islam <rakib.ca@gmail.com>
2024-04-12 13:00:05 -0600

    todo add&delete functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,52 @@
+import { useState } from "react";
+
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    if (todo !== "") {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = todos.filter((todo) => {
+      return todo !== text;
+    });
+    setTodos(newTodos);
+  };
+
   return (
-    <div>
-      <h1>Helloooo</h1>
-    </div>
+    <>
+      <ul>
+        {todos.map((todo, index) => (
+          <li className="list-item" key={index}>
+            <label>
+              <input type="checkbox" />
+            </label>
+            {todo}
+            <button onClick={() => deleteTodo(todo)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <label>
+          New Todo{" "}
+          <input
+            type="text"
+            value={todo}
+            placeholder="Type a Todo HERE"
+            onChange={(e) => {
+              setTodo(e.target.value);
+            }}
+          />
+        </label>
+        <br />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+    </>
   );
 }
 
